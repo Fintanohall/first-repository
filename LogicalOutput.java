f2ea7f3196e9d79a04ef65ca25ed4c824b466fc7
FINTAN O HALLORAN - STUDENT <G00349045@gmit.ie>
2018-02-21 14:05:17 +0000

    test

---
+++ LogicalOutput.java
@@ -0,0 +1,18 @@
+public class LogicalOutput{
+  public static void main(String[] args){
+
+  - i = 1
+  - j = 2
+  - k = 3
+  - m = 2
+
+ System.out.println(i== 1);
+ System.out.println(j==3);
+ System.out.(i>=1) && (j<4));
+ System.out.println((m<= 99) & (k <m>>;
+ System.out.println((j >= i) (k==m));
+ System.out.println((k+m<j) I (3 - j>=k));
+ System.out.println(!(k>m));
+
+ }
+}
